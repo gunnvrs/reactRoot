8807befcbf920e0126a8c1b4827bec6b58b3a2f3
gunnvrs <gunn2545@gmail.com>
2023-02-24 14:30:14 +0700

    start to make a pin function

--- react-router/src/pages/sharing.jsx
+++ react-router/src/pages/sharing.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { signInWithGoogle } from "../Firebase";
-import { collection, addDoc, getDocs, serverTimestamp } from "firebase/firestore";
+import { collection, addDoc, getDocs, serverTimestamp, updateDoc, doc } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { db, auth } from "../Firebase";
 import { v4 } from "uuid";
 import Navbar from "./Navbar";
+
 
 function Share() {
   const [imageUpload, setImageUpload] = useState(null);
@@ -26,12 +27,13 @@ function Share() {
     addDoc(imagesCollectionRef, {
       name: fileName,
       uploadedBy: currentUser.displayName,
-      createdAt: serverTimestamp() // Add current time
+      createdAt: serverTimestamp(),
+      isPinned: false // Add new field isPinned with initial value false
     }).then(() => {
       const storageRef = ref(storage, `images/${fileName}`);
       uploadBytes(storageRef, imageUpload).then(() => {
         getDownloadURL(storageRef).then((url) => {
-          setImageData((prevData) => [...prevData, {url, uploadedBy: currentUser.displayName, createdAt: new Date()}]); // Update state with createdAt field
+          setImageData((prevData) => [...prevData, {url, uploadedBy: currentUser.displayName, createdAt: new Date(), isPinned: false}]); // Update state with createdAt and isPinned fields
         });
       });
     });
@@ -44,11 +46,14 @@ function Share() {
           const storageRef = ref(storage, `images/${doc.data().name}`);
           return getDownloadURL(storageRef).then((url) => {
             const uploadedBy = doc.data().uploadedBy;
-            const createdAt = doc.data().createdAt ? doc.data().createdAt.toDate() : null; // Check if createdAt field exists
-            return { url, uploadedBy, createdAt };
+            const createdAt = doc.data().createdAt ? doc.data().createdAt.toDate() : null;
+            const isPinned = doc.data().isPinned || false;
+            const id = doc.id; // Add id property
+            return { id, url, uploadedBy, createdAt, isPinned };
           });
+          
         });
-  
+    
         Promise.all(promises).then((data) => {
           setImageData(data);
         });
@@ -57,10 +62,35 @@ function Share() {
         console.log(error);
       });
   }, []);
+  
+  
 
-  const handleImageClick = (event) => {
+  const handleImageClick = (event, index) => {
     event.target.classList.toggle("image-selected");
+    const imageDataCopy = [...imageData];
+    const image = imageDataCopy[index];
+    if (image && "isPinned" in image) { // Check if image is defined and has the isPinned property
+      const isPinned = !image.isPinned;
+      image.isPinned = isPinned;
+      const imagesCollectionRef = collection(db, "images");
+      const imageDocRef = doc(imagesCollectionRef, image.id);
+      console.log("Updating isPinned for image with id:", image.id);
+      updateDoc(imageDocRef, { isPinned })
+        .then(() => {
+          console.log("isPinned updated in Firebase");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      setImageData(imageDataCopy);
+    }
   };
+  
+  
+  
+  
+  
+  
 
 
   return (
@@ -76,7 +106,8 @@ function Share() {
         <button onClick={uploadFile}> Upload Image</button>
         {imageData.map((data, index) => (
           <div key={index}>
-            <img src={data.url} className="image" onClick={handleImageClick} />
+            <img src={data.url} className="image" onClick={(event) => handleImageClick(event, index)} />
+
             {data.createdAt && <h5>Added by: {data.uploadedBy} at {data.createdAt.toLocaleString()}</h5>}
 
           </div>
